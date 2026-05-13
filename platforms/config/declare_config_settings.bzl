ArchangelX360/toolchains_llvm_bootstrapped

Add MSVC support

platforms/config/declare_config_settings.bzl
@@ -1,6 +1,7 @@
 load("@bazel_skylib//lib:selects.bzl", "selects")
+load("//constraints/abi:abis.bzl", "ABIS")
 load("//constraints/libc:libc_versions.bzl", "GLIBCS", "LIBCS")
-load("//platforms:common.bzl", "LIBC_SUPPORTED_TARGETS", "SUPPORTED_TARGETS")
+load("//platforms:common.bzl", "ABI_SUPPORTED_TARGETS", "LIBC_SUPPORTED_TARGETS", "SUPPORTED_TARGETS")
 
 def declare_config_settings():
     for (target_os, target_cpu) in SUPPORTED_TARGETS:
@@ -26,6 +27,7 @@ def declare_config_settings():
         )
 
     declare_config_settings_libc_aware()
+    declare_config_settings_abi_aware()
 
 def declare_config_settings_libc_aware():
     for (target_os, target_cpu) in LIBC_SUPPORTED_TARGETS:
@@ -69,3 +71,24 @@ def declare_config_settings_libc_aware():
         ],
         visibility = ["//visibility:public"],
     )
+
+def declare_config_settings_abi_aware():
+    for abi in ABIS + ["unconstrained"]:
+        for (target_os, target_cpu) in ABI_SUPPORTED_TARGETS:
+            native.config_setting(
+                name = "{}_{}_{}".format(target_os, target_cpu, abi),
+                constraint_values = [
+                    "@platforms//cpu:{}".format(target_cpu),
+                    "@platforms//os:{}".format(target_os),
+                    "//constraints/abi:{}".format(abi),
+                ],
+                visibility = ["//visibility:public"],
+            )
+
+        native.config_setting(
+            name = "abi_{}".format(abi),
+            constraint_values = [
+                "//constraints/abi:{}".format(abi),
+            ],
+            visibility = ["//visibility:public"],
+        )

platforms/declare_platforms.bzl
@@ -1,5 +1,6 @@
+load("//constraints/abi:abis.bzl", "ABIS", "DEFAULT_ABI")
 load("//constraints/libc:libc_versions.bzl", "LIBCS", "default_libc")
-load("//platforms:common.bzl", "ARCH_ALIASES", "LIBC_SUPPORTED_TARGETS", "SUPPORTED_TARGETS")
+load("//platforms:common.bzl", "ABI_SUPPORTED_TARGETS", "ARCH_ALIASES", "LIBC_SUPPORTED_TARGETS", "SUPPORTED_TARGETS")
 
 def declare_platforms():
     for (target_os, target_cpu) in SUPPORTED_TARGETS:
@@ -19,6 +20,17 @@ def declare_platforms():
             # constraint if they want to.
             constraints.append("//constraints/libc:{}".format(default_libc(target_os, target_cpu)))
 
+        if target_os == "windows":
+            # We add a default abi constraint for windows platforms.
+            #
+            # This is needed because some toolchains require an abi constraint
+            # to be present on the platform in order to select the right
+            # toolchain implementation.
+            #
+            # Users can still create their own platforms without an abi
+            # constraint if they want to.
+            constraints.append("//constraints/abi:{}".format(DEFAULT_ABI))
+
         native.platform(
             name = "{}_{}".format(target_os, target_cpu),
             constraint_values = constraints,
@@ -33,6 +45,7 @@ def declare_platforms():
             )
 
     declare_platforms_libc_aware()
+    declare_platforms_abi_aware()
 
 def declare_platforms_libc_aware():
     for target_os, target_cpu in LIBC_SUPPORTED_TARGETS:
@@ -54,6 +67,30 @@ def declare_platforms_libc_aware():
                         "@platforms//cpu:{}".format(target_cpu),
                         "@platforms//os:{}".format(target_os),
                         "//constraints/libc:{}".format(libc),
+                    ],
+                    visibility = ["//visibility:public"],
+                )
+
+def declare_platforms_abi_aware():
+    for target_os, target_cpu in ABI_SUPPORTED_TARGETS:
+        for abi in ABIS:
+            native.platform(
+                name = "{}_{}_{}".format(target_os, target_cpu, abi),
+                constraint_values = [
+                    "@platforms//cpu:{}".format(target_cpu),
+                    "@platforms//os:{}".format(target_os),
+                    "//constraints/abi:{}".format(abi),
+                ],
+                visibility = ["//visibility:public"],
+            )
+
+            for alias in ARCH_ALIASES.get(target_cpu, []):
+                native.platform(
+                    name = "{}_{}_{}".format(target_os, alias, abi),
+                    constraint_values = [
+                        "@platforms//cpu:{}".format(target_cpu),
+                        "@platforms//os:{}".format(target_os),
+                        "//constraints/abi:{}".format(abi),
                     ],
                     visibility = ["//visibility:public"],
                 )

hermeticbuild/hermetic-llvm

platforms/config/declare_config_settings.bzl
@@ -1,6 +1,7 @@
 load("@bazel_skylib//lib:selects.bzl", "selects")
 load("//constraints/libc:libc_versions.bzl", "GLIBCS", "LIBCS")
-load("//platforms:common.bzl", "LIBC_SUPPORTED_TARGETS", "SUPPORTED_TARGETS")
+load("//constraints/windows/abi:abis.bzl", "WINDOWS_ABIS")
+load("//platforms:common.bzl", "LIBC_SUPPORTED_TARGETS", "SUPPORTED_TARGETS", "WINDOWS_ABI_SUPPORTED_TARGETS")
 
 def declare_config_settings():
     for (target_os, target_cpu) in SUPPORTED_TARGETS:
@@ -26,6 +27,7 @@ def declare_config_settings():
         )
 
     declare_config_settings_libc_aware()
+    declare_config_settings_windows_abi_aware()
 
 def declare_config_settings_libc_aware():
     for (target_os, target_cpu) in LIBC_SUPPORTED_TARGETS:
@@ -69,3 +71,16 @@ def declare_config_settings_libc_aware():
         ],
         visibility = ["//visibility:public"],
     )
+
+def declare_config_settings_windows_abi_aware():
+    for abi in WINDOWS_ABIS:
+        for (target_os, target_cpu) in WINDOWS_ABI_SUPPORTED_TARGETS:
+            native.config_setting(
+                name = "{}_{}_{}".format(target_os, target_cpu, abi),
+                constraint_values = [
+                    "@platforms//cpu:{}".format(target_cpu),
+                    "@platforms//os:{}".format(target_os),
+                    "//constraints/windows/abi:{}".format(abi),
+                ],
+                visibility = ["//visibility:public"],
+            )

platforms/declare_platforms.bzl
@@ -1,5 +1,6 @@
 load("//constraints/libc:libc_versions.bzl", "LIBCS", "default_libc")
-load("//platforms:common.bzl", "ARCH_ALIASES", "LIBC_SUPPORTED_TARGETS", "SUPPORTED_TARGETS")
+load("//constraints/windows/abi:abis.bzl", "UNCONSTRAINED_WINDOWS_ABI", "WINDOWS_ABIS", "WINDOWS_DEFAULT_ABI")
+load("//platforms:common.bzl", "ARCH_ALIASES", "LIBC_SUPPORTED_TARGETS", "SUPPORTED_TARGETS", "WINDOWS_ABI_SUPPORTED_TARGETS")
 
 def declare_platforms():
     for (target_os, target_cpu) in SUPPORTED_TARGETS:
@@ -19,6 +20,17 @@ def declare_platforms():
             # constraint if they want to.
             constraints.append("//constraints/libc:{}".format(default_libc(target_os, target_cpu)))
 
+        if target_os == "windows":
+            # We add a default abi constraint for windows platforms.
+            #
+            # This is needed because some toolchains require an abi constraint
+            # to be present on the platform in order to select the right
+            # toolchain implementation.
+            #
+            # Users can still create their own platforms without an abi
+            # constraint if they want to.
+            constraints.append("//constraints/windows/abi:{}".format(WINDOWS_DEFAULT_ABI))
+
         native.platform(
             name = "{}_{}".format(target_os, target_cpu),
             constraint_values = constraints,
@@ -33,6 +45,7 @@ def declare_platforms():
             )
 
     declare_platforms_libc_aware()
+    declare_platforms_windows_abi_aware()
 
 def declare_platforms_libc_aware():
     for target_os, target_cpu in LIBC_SUPPORTED_TARGETS:
@@ -54,6 +67,30 @@ def declare_platforms_libc_aware():
                         "@platforms//cpu:{}".format(target_cpu),
                         "@platforms//os:{}".format(target_os),
                         "//constraints/libc:{}".format(libc),
+                    ],
+                    visibility = ["//visibility:public"],
+                )
+
+def declare_platforms_windows_abi_aware():
+    for target_os, target_cpu in WINDOWS_ABI_SUPPORTED_TARGETS:
+        for abi in WINDOWS_ABIS:
+            native.platform(
+                name = "{}_{}_{}".format(target_os, target_cpu, abi),
+                constraint_values = [
+                    "@platforms//cpu:{}".format(target_cpu),
+                    "@platforms//os:{}".format(target_os),
+                    "//constraints/windows/abi:{}".format(abi),
+                ],
+                visibility = ["//visibility:public"],
+            )
+
+            for alias in ARCH_ALIASES.get(target_cpu, []):
+                native.platform(
+                    name = "{}_{}_{}".format(target_os, alias, abi),
+                    constraint_values = [
+                        "@platforms//cpu:{}".format(target_cpu),
+                        "@platforms//os:{}".format(target_os),
+                        "//constraints/windows/abi:{}".format(abi),
                     ],
                     visibility = ["//visibility:public"],
                 )