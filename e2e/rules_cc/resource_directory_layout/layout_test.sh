4e727c600d4902332abaf958d4f8dcc052b7d62f
Corentin Kerisit <corentin.kerisit@gmail.com>
2026-09-15 15:52:42 +0200

    Remove resource directory layout test
    Co-authored-by: Fabian Meumertzheim <fabian@meumertzhe.im>
    Co-authored-by: Keith Smiley <keithbsmiley@gmail.com>

--- e2e/rules_cc/resource_directory_layout/layout_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-for resource in "$@"; do
-  [[ $(<"$resource/include/builtin.h") == "// compiler header" ]]
-  [[ $(<"$resource/share/ignorelist.txt") == "compiler ignorelist" ]]
-  [[ $(<"$resource/lib/target/sentinel.txt") == "target runtime" ]]
-  [[ ! -e "$resource/lib/host/sentinel.txt" ]]
-done
